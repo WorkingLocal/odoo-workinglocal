ad4f6132c76b0b58e48e41dc925ba40a03cd6d05
WorkingLocal <dev@workinglocal.be>
2026-07-06 09:45:59 +0200

    feat(workinglocal_theme): uitgebreide Odoo-huisstijl — login/website/rapport SCSS, menu, mail templates, company/website data XML

--- addons/workinglocal_theme/__manifest__.py
+++ addons/workinglocal_theme/__manifest__.py
@@ -1,18 +1,31 @@
 {
     'name': 'Working Local Theme',
-    'version': '19.0.1.0.0',
+    'version': '19.0.2.0.0',
     'category': 'Theme',
-    'summary': 'Working Local huisstijl voor Odoo backend',
+    'summary': 'Working Local huisstijl — backend, website, documenten en e-mail',
     'author': 'Working Local',
     'website': 'https://workinglocal.be',
-    'depends': ['web'],
+    'depends': ['web', 'website', 'account', 'mail'],
+    'data': [
+        'data/company_data.xml',
+        'views/webclient_templates.xml',
+        'views/mail_templates.xml',
+        'views/menu_data.xml',
+        'views/website_pages.xml',
+        'data/website_data.xml',
+    ],
     'assets': {
         'web.assets_backend': [
-            # Prepend: onze variabelen VOOR Odoo pre_variables laden
             ('prepend', 'workinglocal_theme/static/src/scss/variables.scss'),
+            'workinglocal_theme/static/src/scss/workinglocal_theme.scss',
         ],
-        'web.assets_backend_lazy': [
+        'web.assets_frontend': [
             ('prepend', 'workinglocal_theme/static/src/scss/variables.scss'),
+            'workinglocal_theme/static/src/scss/login.scss',
+            'workinglocal_theme/static/src/scss/website.scss',
+        ],
+        'web.report_assets_common': [
+            'workinglocal_theme/static/src/scss/report.scss',
         ],
     },
     'installable': True,
